d9b5e3298d9907b18a09813b93f8e6798ee732c9
lychautrinha@gmail.com <nhalct@fireapps.vn>
2022-10-21 15:23:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { getAbsoluteAdminUrl } = require('strapi-utils/lib');
 const app = express();
  
+const dataTest = {
+    "full_name": "Nhã nè",
+    "email": "nha@gmail.com"
+}
+
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('Hello server is running')
+    .json(dataTest)
     .end();
 });
  
