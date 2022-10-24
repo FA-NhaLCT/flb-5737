d6ecf5fc35c1e38c8444320d7d854d3f40fadbcb
lychautrinha@gmail.com <nhalct@fireapps.vn>
2022-10-24 11:46:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ const dataTest = {
 }
 
 app.get('/', (req, res) => {
-    res
-        .status(200)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.status(200)
         .json(dataTest)
         .end();
 });
